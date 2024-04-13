8f7f5e6f50dad1987be36179149bd9eee7a96c84
kelly6bf <kelly6bf@gmail.com>
2024-04-14 04:58:09 +0900

    Init Project

---
+++ src/main/java/site/study/boardtraining/BoardTrainingApplication.java
@@ -0,0 +1,13 @@
+package site.study.boardtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/study/boardtraining/BoardTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package site.study.boardtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
